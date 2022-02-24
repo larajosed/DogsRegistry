36821202e9f413beb042573602eb632cd861aedf
Jose Daniel Lara <larajosed@gmail.com>
2022-02-24 19:32:32 +0100

    Service to retrieve all dogs and one dog per id

---
+++ app/Http/Controllers/DogController.php
@@ -0,0 +1,26 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Dog;
+use App\Http\Controllers\Controller;
+ 
+class DogController extends Controller
+{
+    /**
+     * Get the dog with by the given id.
+     *
+     * @param  int  $id
+     */
+    public function get($id) 
+    {
+        return Dog::findOrFail($id);
+    }
+
+    /**
+     * Get all the dogs
+     */
+     public function getAll(){
+         return Dog::all();
+     }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,5 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::get('dogs', 'DogController@getAll');
+Route::get('dogs/{id}', 'DogController@get');
